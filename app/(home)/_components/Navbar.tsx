7e1ea06dd061869574c3b5a90d3d23bf79619248
Saad Sayyed <saadsyed950@gmail.com>
2024-12-14 22:41:52 +0530

    Day, DD/MM/YY working correctly

--- app/(home)/_components/Navbar.tsx
+++ app/(home)/_components/Navbar.tsx
@@ -5,8 +5,9 @@ import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const [currentTime, setCurrentTime] = useState<string>("");
+  const [currentDate, setCurrentDate] = useState<string>("");
 
-  const formatDate = () => {
+  const formatTime = () => {
     const now = new Date();
     return new Intl.DateTimeFormat("en-US", {
       hour: "numeric",
@@ -15,11 +16,25 @@ const Navbar = () => {
     }).format(now);
   };
 
+  const formatDate = () => {
+    const now = new Date();
+
+    const day = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(
+      now
+    );
+    const date = String(now.getDay()).padStart(2, "0");
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const year = String(now.getFullYear()).slice(-2);
+
+    return `${day}, ${date}/${month}/${year}`;
+  };
+
   useEffect(() => {
-    setCurrentTime(formatDate());
+    setCurrentDate(formatDate());
+    setCurrentTime(formatTime());
 
     const interval = setInterval(() => {
-      setCurrentTime(formatDate());
+      setCurrentTime(formatTime());
     }, 60000);
 
     return () => clearInterval(interval);
@@ -28,7 +43,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center px-10 py-4">
       <h1 className="font-agrandir lg:text-4xl font-semibold tracking-wider">
-        {currentTime}
+        {currentDate}
       </h1>
       <Switch />
     </nav>
