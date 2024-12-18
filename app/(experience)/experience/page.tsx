dcb1a2c61998e2d2104fbb56702a007d5855ca62
Saad Sayyed <saadsyed950@gmail.com>
2024-12-18 13:16:53 +0530

    Done for morning sessh

--- app/(experience)/experience/page.tsx
+++ app/(experience)/experience/page.tsx
@@ -1,10 +1,9 @@
-import { Rocket, Users } from "lucide-react";
 import ExperienceComponent from "./_components/ExperienceComponent";
 
 const WorkPage = () => {
   return (
     <div className="flex flex-col gap-y-10 justify-center items-center w-full min-h-screen">
-      <h1 className="lg:text-4xl text-xl font-semibold capitalize font-agrandir text-center tracking-wide">
+      <h1 className="lg:text-6xl text-4xl font-semibold capitalize font-agrandir text-center tracking-wide">
         My Experience
       </h1>
       <ExperienceComponent />
