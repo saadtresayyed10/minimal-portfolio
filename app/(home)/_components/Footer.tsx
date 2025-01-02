a4c9612154b9675a5a940bbf6eddaf136bc01e62
Saad Sayyed <saadsyed950@gmail.com>
2025-01-02 12:20:12 +0530

    footer animation done

--- app/(home)/_components/Footer.tsx
+++ app/(home)/_components/Footer.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "framer-motion";
 import {
   IconBrandGithub,
   IconBrandLinkedin,
@@ -9,7 +12,12 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="flex justify-between items-center px-6 py-4">
-      <div className="flex justify-center items-center lg:gap-x-5 gap-x-2">
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, ease: "backOut", delay: 0.4 }}
+        className="flex justify-center items-center lg:gap-x-5 gap-x-2"
+      >
         <Link
           target="_blank"
           href="https://www.linkedin.com/in/saad-sayyed-trev/"
@@ -29,8 +37,13 @@ const Footer = () => {
         <Link target="_blank" href="https://www.instagram.com/sameoldtreva">
           <IconBrandInstagram className="w-5 h-5 text-neutral-800 dark:text-neutral-100 stroke-[1.5]" />
         </Link>
-      </div>
-      <div className="flex justify-center items-center lg:gap-x-10 gap-x-6">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, ease: "backOut", delay: 0.4 }}
+        className="flex justify-center items-center lg:gap-x-10 gap-x-6"
+      >
         <Link target="_blank" href="/images/Saad_Sayyed_Resume.pdf">
           <p className="font-agrandir text-xs font-light tracking-wider">
             Resume
@@ -41,7 +54,7 @@ const Footer = () => {
             Agency
           </p>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 };
